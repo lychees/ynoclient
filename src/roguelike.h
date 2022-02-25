a53531d79a48e716ef8638176bfa920d8d93979f
xiaodao <lychees67@gmail.com>
2022-02-26 04:27:18 +0800

    Update roguelike.h

--- src/roguelike.h
+++ src/roguelike.h
@@ -6,8 +6,8 @@
 using std::vector;
 
 namespace Roguelike {
-	vector<vector<int> > Gen(int h, int w) {
-		vector<int> z; z.resize(h);
+	vector<vector<int>> Gen(int h, int w) {
+		vector<vector<int>> z; z.resize(h);
 		for (int i=0;i<h;++i) {
 			for (int j=0;j<w;++j) {
 				z[i].push_back((std::rand() & 1) ? 1 : 0);
