bf7830b5dc0fbf17ed1beb412a51f739a6c7d703
xiaodao <lychees67@gmail.com>
2022-02-26 20:06:39 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,18 @@
 
 
 
-#include "Engine.hpp"
+#include "libtcod.hpp"
+class Engine {
+public :
+    //TCODList<Actor *> actors;
+   // Actor *player;
+   // Map *map;
+
+    Engine();
+    ~Engine();
+    void update();
+    void render();
+};
 namespace Roguelike {
 	Engine engine;
 }
