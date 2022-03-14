e27424c6619d6ca07fe46c9f577141cb9705fb59
xiaodao <lychees67@gmail.com>
2022-03-15 02:49:03 +0800

    Update tilemap_layer.cpp

--- src/tilemap_layer.cpp
+++ src/tilemap_layer.cpp
@@ -206,6 +206,9 @@ static uint32_t MakeAbTileHash(int id, int anim_step) {
 }
 
 void TilemapLayer::Draw(Bitmap& dst, int z_order) {
+
+	dst = *DisplayUi->GetDisplaySurface();
+
 	// Get the number of tiles that can be displayed on window
 	int tiles_x = (int)ceil(SCREEN_TARGET_WIDTH / (float)TILE_SIZE);
 	int tiles_y = (int)ceil(SCREEN_TARGET_HEIGHT / (float)TILE_SIZE);
