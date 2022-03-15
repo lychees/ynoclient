1965e064b42ab54bd4b555c909cc43a371bc8ddf
xiaodao <lychees67@gmail.com>
2022-03-15 21:17:03 +0800

    Update graphics.cpp

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -128,9 +128,9 @@ void Graphics::Draw(Bitmap& dst) {
 	//pixman_image_set_transform(dst.bitmap.get(), &xform.matrix);
 	// ?
 
-	auto &dst = *DisplayUi->GetDisplaySurface();
+	auto &dstt = *DisplayUi->GetDisplaySurface();
 	Transform xform = Transform::Scale(0.5, 0.5);
-	pixman_image_set_transform(dst.bitmap.get(), &xform.matrix);
+	pixman_image_set_transform(dstt.bitmap.get(), &xform.matrix);
 }
 
 void Graphics::LocalDraw(Bitmap& dst, int min_z, int max_z) {
