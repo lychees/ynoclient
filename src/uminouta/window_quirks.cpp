f1db50abc00cc075bb43232d63694eee57e1cd0b
xiaodao <lychees67@gmail.com>
2022-03-23 22:35:48 +0800

    Update window_quirks.cpp

--- src/uminouta/window_quirks.cpp
+++ src/uminouta/window_quirks.cpp
@@ -78,7 +78,7 @@ void Window_Quirks::DrawItem(int id) {
 
 	// contents->TextDraw(rect.x + rect.width - 24, rect.y, color, player.buffs[id]);
 	contents->TextDraw(rect.x, rect.y, color, player.quirks[id].first);
-	contents->TextDraw(rect.x, rect.y, color, "Lv " + std::to_string(player.quirks[id].second), Text::AlignRgiht);
+	contents->TextDraw(rect.x, rect.y, color, "Lv " + std::to_string(player.quirks[id].second), Text::AlignRight);
 
 	/*fmt::format("{}{:3d}", lcf::rpg::Terms::TermOrDefault(lcf::Data::terms.easyrpg_skill_cost_separator, "-"), costs) );*/
 	// Skills are guaranteed to be valid
