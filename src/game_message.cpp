b56c1ac03853fc46135acfc625d8bfb0229e68f1
xiaodao <lychees67@gmail.com>
2022-03-27 23:54:06 +0800

    Update game_message.cpp

--- src/game_message.cpp
+++ src/game_message.cpp
@@ -84,7 +84,7 @@ int Game_Message::WordWrap(StringView line, const int limit, const WordWrapCallb
 	int line_count = 0;
 	FontRef font = Font::Default();
 
-	Debug::Output("limit: ", limit);
+	Output::Debug("limit: {}", limit);
 
 	do {
 		int next = start;
