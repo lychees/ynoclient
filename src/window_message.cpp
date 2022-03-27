2f37eba6790efbe74bdd92c35aae3b420e5e7400
xiaodao <lychees67@gmail.com>
2022-03-28 00:05:45 +0800

    Update window_message.cpp

--- src/window_message.cpp
+++ src/window_message.cpp
@@ -164,7 +164,7 @@ void Window_Message::StartMessageProcessing(PendingMessage pm) {
 			/* TODO: don't take commands like \> \< into account when word-wrapping */
 			Game_Message::WordWrap(
 					line,
-					50
+					50,
 					//width - 24,
 					[&](StringView wrapped_line) {
 						append(std::string(wrapped_line));
